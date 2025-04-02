a18f532e6e2f88fb1cf3a65b73d33d77ec62ef37
SimerusM <63352151+SimerusM@users.noreply.github.com>
2025-04-01 20:47:00 -0400

    test change to url

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
 
 # Database configuration
 # Ensure this points to your PostgreSQL database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost:5432/your_database_name')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
